854f38f23885dde4f5593dba970956623006ef43
Mohd Adnatull Al Masum <Adnatull@users.noreply.github.com>
2018-08-12 14:31:09 +0600

    Update Tokenization.cpp

--- Compiler Design Laboratory/Tokenization.cpp
+++ Compiler Design Laboratory/Tokenization.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
@@ -32,11 +33,11 @@ private:
 
     void searchToken (token *tree, string tokenname, string value) {
         if(tree==NULL) {
-            cout << "The token " << tokenname << " and value " << value << "do not exist in the binary search tree." << endl;
+            cout << "The token " << tokenname << " and value " << value << " do not exist in the binary search tree." << endl;
             return;
         }
         else if (tree->value==value && tree->tokenname == tokenname) {
-            cout << "The token " << tokenname << " and value " << value << "do exist in the binary search tree." << endl;
+            cout << "The token " << tokenname << " and value " << value << " do exist in the binary search tree." << endl;
             return;
         }
         else searchToken (tree->next, tokenname, value);
@@ -45,7 +46,7 @@ private:
 
     token* deleteToken(token *tree, string tokenname, string value) {
         if(tree==NULL) {
-            cout << "The token " << tokenname << " and value " << value << "do not exist in the binary search tree." << endl;
+            cout << "The token " << tokenname << " and value " << value << " do not exist in the binary search tree." << endl;
         }
         else if (tree->value==value && tree->tokenname == tokenname) {
             if(tree->next==NULL) {
@@ -63,15 +64,17 @@ private:
 
     void display() {
         token* temp = head;
-
+        int cnt = 0;
         while(temp!=NULL) {
             cout <<  temp->tokenname << "," << temp->value ;
+            cnt++;
             if (temp->next != NULL) {
                 cout << " => ";
             }
             temp = temp->next;
         }
-        cout << endl << endl;
+        if(cnt)
+            cout << endl;
     }
 
     public:
@@ -87,12 +90,12 @@ private:
 
         void searchTOKEN (string tokenname, string value) {
             token *temp = head;
-            return searchToken (temp,tokenname, value);
+            searchToken (temp,tokenname, value);
         }
 
-        token* deleteTOKEN( string tokenname, string value) {
+        void deleteTOKEN( string tokenname, string value) {
             token *temp = head;
-            return deleteToken(temp, tokenname, value);
+            head = deleteToken(temp, tokenname, value);
         }
 
         void DISPLAY() {
@@ -104,7 +107,43 @@ private:
 };
 
 int main() {
-    freopen ("input.txt", "r", stdin);
+
+    cout << "Insert 5 to input token 6 to delete, 7 to search, 8 to print and 9 to exit" << endl;
+    linkedList Table[5];
+    string p, q;
+    int n;
+    while (cin >> n) {
+        cin.ignore();
+        if ( n== 5) {
+            cout << "Input token and value to insert, give space between them " << endl;
+            cin >> p >> q;
+            if (p=="DT") Table[0].addTOKEN( p, q);
+            else if (p == "VAR") Table[1].addTOKEN (p, q);
+            else if (p == "NUM") Table[2].addTOKEN( p, q);
+        } else if ( n == 6) {
+            cout << "Input token and value to delete, give space between them" << endl;
+            cin >> p >> q;
+            if (p=="DT") Table[0].deleteTOKEN( p, q);
+            else if (p == "VAR") Table[1].deleteTOKEN(p, q);
+            else if (p == "NUM") Table[2].deleteTOKEN( p, q);
+        } else if ( n == 7) {
+            cout << "Input token and value to search, give space between them" << endl;
+            cin >> p >> q;
+            if (p=="DT") Table[0].searchTOKEN( p, q);
+            else if (p == "VAR") Table[1].searchTOKEN(p, q);
+            else if (p == "NUM") Table[2].searchTOKEN( p, q);
+        } else if (n == 8) {
+            for (int i=0; i<3; i++) {
+                Table[i].DISPLAY();
+            }
+        } else {
+            break;
+        }
+
+    }
+
+
+   /* freopen ("input.txt", "r", stdin);
     string s;
     linkedList Table[5];
     while (getline(cin, s)) {
@@ -125,6 +164,6 @@ int main() {
     }
     Table[0].DISPLAY();
     Table[1].DISPLAY();
-    Table[2].DISPLAY();
+    Table[2].DISPLAY();*/
     return 0;
 }
